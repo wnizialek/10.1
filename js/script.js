09aa3a8245be6e7b5382cf3a921d8ed0034b5547
Wojciech Nizialek <wojciech.nizialek@gmail.com>
2018-11-19 19:47:16 +0000

    Add mustache

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+var slidesList = document.getElementById('slides-list').innerHTML;
+var results = document.getElementById('results');
+
+for(var i = 0; i < slidesData.length; i++){
+    results.insertAdjacentHTML('beforeend', Mustache.render(slidesList, slidesData[i]));
+}
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
@@ -5,4 +12,5 @@ var flkty = new Flickity( elem, {
   contain: true,
   hash: true
 });
+
 
